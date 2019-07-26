159dbb0b847b97ec32ae82c922086a328f9ad6c8
Rob Smith <merlin@sleepingkingstudios.com>
2019-07-26 14:41:31 -0400

    Implement SpellSerializer.

---
+++ app/serializers/spell_serializer.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+# Serializes a spell as a JSON-compatible hash.
+class SpellSerializer < RecordSerializer
+  attributes \
+    :casting_time,
+    :description,
+    :duration,
+    :level,
+    :material_component,
+    :name,
+    :range,
+    :ritual,
+    :school,
+    :somatic_component,
+    :verbal_component
+
+  alias_method :spell, :object
+end
---
+++ spec/serializers/spell_serializer_spec.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+require 'support/examples/serializer_examples'
+
+RSpec.describe SpellSerializer do
+  include Spec::Support::Examples::SerializerExamples
+
+  subject(:serializer) { described_class.new }
+
+  let(:spell) { FactoryBot.create(:spell) }
+
+  describe '::new' do
+    it { expect(described_class).to be_constructible.with(0).arguments }
+  end
+
+  describe '#serialize' do
+    let(:object) { spell }
+
+    it { expect(serializer).to respond_to(:serialize).with(1).argument }
+
+    include_examples 'should serialize attributes',
+      :id,
+      :casting_time,
+      :description,
+      :duration,
+      :level,
+      :material_component,
+      :name,
+      :range,
+      :ritual,
+      :school,
+      :somatic_component,
+      :verbal_component
+  end
+end
