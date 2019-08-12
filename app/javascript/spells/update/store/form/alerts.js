fea2df205ae222193be270c2451c1231523cd2cf
Rob Smith <merlin@sleepingkingstudios.com>
2019-08-12 11:15:13 -0400

    Implement update Spell JS form store.

---
+++ app/javascript/spells/update/store/form/alerts.js
@@ -0,0 +1,33 @@
+import { addAlert } from '../../../../components/alerts/store/actions';
+import { generateFingerprintUuid } from '../../../../utils/uuid';
+
+const failureAlert = {
+  id: generateFingerprintUuid('spells/update'),
+  alertStyle: 'warning',
+  dismissible: true,
+  message: 'Unable to update spell.',
+};
+
+const successAlert = {
+  id: generateFingerprintUuid('spells/update'),
+  alertStyle: 'success',
+  dismissible: true,
+  message: 'Successfully updated spell.',
+};
+
+export default {
+  handleFailure: next => ({ dispatch, getState, response }) => {
+    next({ dispatch, getState, response });
+
+    const action = addAlert(failureAlert);
+
+    dispatch(action);
+  },
+  handleSuccess: next => ({ dispatch, getState, response }) => {
+    next({ dispatch, getState, response });
+
+    const action = addAlert(successAlert);
+
+    dispatch(action);
+  },
+};
---
+++ app/javascript/spells/update/store/form/index.js
@@ -0,0 +1,22 @@
+import submitForm from '../../../../requests/form';
+import { buildSpell } from '../../../entities';
+import alerts from './alerts';
+import redirect from './redirect';
+
+const namespace = 'updateSpellForm';
+const REQUEST_URL = '/api/spells/:id';
+
+export const {
+  actions,
+  reducer,
+  request,
+} = submitForm({
+  data: { spell: buildSpell() },
+  method: 'PATCH',
+  middleware: [
+    redirect,
+    alerts,
+  ],
+  namespace,
+  url: REQUEST_URL,
+});
