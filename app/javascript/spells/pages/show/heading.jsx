eb23c4f8affddf1055c878379640cda24626eb23
Rob Smith <merlin@sleepingkingstudios.com>
2019-08-23 14:44:00 -0400

    Implement show Spell page heading.

---
+++ app/javascript/spells/pages/show/heading.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import HeadingWithButtons from '../../../components/heading-with-buttons';
+import { useSpell } from '../../show/store';
+
+const generateButtons = ({ spell }) => {
+  if (!(spell && spell.id)) { return []; }
+
+  return [
+    {
+      label: 'Update Spell',
+      outline: true,
+      url: `/spells/${spell.id}/update`,
+    },
+  ];
+};
+
+const ShowSpellHeading = () => {
+  const buttons = useSpell(generateButtons);
+
+  return (
+    <HeadingWithButtons buttons={buttons}>
+      Show Spell
+    </HeadingWithButtons>
+  );
+};
+
+ShowSpellHeading.defaultProps = {};
+
+ShowSpellHeading.propTypes = {};
+
+export default ShowSpellHeading;
