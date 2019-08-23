c3a9e8de0682f9a22a39ea40b3af620908a23d22
Rob Smith <merlin@sleepingkingstudios.com>
2019-08-23 17:28:33 -0400

    Remove legacy show Spell page.

---
+++ app/javascript/spells/pages/show/page.test.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import ShowSpellBreadcrumbs from './breadcrumbs';
+import ShowSpellPage from './page';
+import { findSpell } from '../../show/store';
+
+jest.mock('../../show/store');
+
+describe('ShowSpellPage', () => {
+  const id = '00000000-0000-0000-0000-000000000000';
+  const match = { params: { id } };
+  const defaultProps = { match };
+
+  it('should render the Page', () => {
+    const rendered = shallow(<ShowSpellPage {...defaultProps} />);
+
+    expect(rendered).toHaveDisplayName('Page');
+    expect(rendered).toHaveClassName('page-spells');
+    expect(rendered).toHaveProp('breadcrumbs', (<ShowSpellBreadcrumbs />));
+  });
+
+  it('should render the heading', () => {
+    const rendered = shallow(<ShowSpellPage {...defaultProps} />);
+
+    expect(rendered).toContainMatchingElement('ShowSpellHeading');
+  });
+
+  it('should render the spell block', () => {
+    const rendered = shallow(<ShowSpellPage {...defaultProps} />);
+
+    expect(rendered).toContainMatchingElement('ShowSpellBlock');
+  });
+
+  it('should find the spell by id', () => {
+    shallow(<ShowSpellPage {...defaultProps} />);
+
+    expect(findSpell).toHaveBeenCalledWith(id);
+  });
+});
