49ecb16746f0179670748c939f610baf454eacc9
Rob Smith <merlin@sleepingkingstudios.com>
2019-08-31 22:38:53 -0400

    Add delete button to Spell index.

--- app/javascript/components/button/index.jsx
+++ app/javascript/components/button/index.jsx
@@ -5,17 +5,21 @@ import { buttonClass, disabledOnClick } from './utils';
 
 const Button = ({
   block,
+  buttonSize,
   buttonStyle,
   children,
   className,
   disabled,
+  link,
   onClick,
   outline,
 }) => {
   const generatedClassName = buttonClass({
     block,
+    buttonSize,
     buttonStyle,
     className,
+    link,
     outline,
   });
 
@@ -33,18 +37,22 @@ const Button = ({
 
 Button.defaultProps = {
   block: false,
+  buttonSize: 'md',
   buttonStyle: 'primary',
   className: null,
   disabled: false,
+  link: false,
   outline: false,
 };
 
 Button.propTypes = {
   block: PropTypes.bool,
+  buttonSize: PropTypes.string,
   buttonStyle: PropTypes.string,
   children: PropTypes.node.isRequired,
   className: PropTypes.string,
   disabled: PropTypes.bool,
+  link: PropTypes.bool,
   onClick: PropTypes.func.isRequired,
   outline: PropTypes.bool,
 };
--- app/javascript/components/link-button/index.jsx
+++ app/javascript/components/link-button/index.jsx
@@ -6,16 +6,20 @@ import { buttonClass } from '../button/utils';
 
 const LinkButton = ({
   block,
+  buttonSize,
   buttonStyle,
   children,
   className,
+  link,
   outline,
   url,
 }) => {
   const generatedClassName = buttonClass({
     block,
+    buttonSize,
     buttonStyle,
     className,
+    link,
     outline,
   });
 
@@ -28,16 +32,20 @@ const LinkButton = ({
 
 LinkButton.defaultProps = {
   block: false,
+  buttonSize: null,
   buttonStyle: 'primary',
   className: null,
+  link: false,
   outline: false,
 };
 
 LinkButton.propTypes = {
   block: PropTypes.bool,
+  buttonSize: PropTypes.string,
   buttonStyle: PropTypes.string,
   children: PropTypes.node.isRequired,
   className: PropTypes.string,
+  link: false,
   outline: PropTypes.bool,
   url: PropTypes.string.isRequired,
 };
