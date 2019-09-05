eb7e930dfc64d673abb8a68a8636494fc88dadb7
Rob Smith <merlin@sleepingkingstudios.com>
2019-09-05 01:05:41 -0400

    Implement UpdateFindPublication store.

--- app/javascript/publications/store/index.js
+++ app/javascript/publications/store/index.js
@@ -3,9 +3,11 @@ import { combineReducers } from 'redux';
 import { reducer as createPublicationForm } from './createPublicationForm';
 import { reducer as indexFindPublications } from './indexFindPublications';
 import { reducer as showFindPublication } from './showFindPublication';
+import { reducer as updatePublicationForm } from './updatePublicationForm';
 
 export default combineReducers({
   createPublicationForm,
   indexFindPublications,
   showFindPublication,
+  updatePublicationForm,
 });
