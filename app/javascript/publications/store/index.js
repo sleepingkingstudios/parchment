a94a52408896e840cbf209bc6884eddbf9d1b79a
Rob Smith <merlin@sleepingkingstudios.com>
2019-09-04 22:09:50 -0400

    Implement ShowFindPublication store.

--- app/javascript/publications/store/index.js
+++ app/javascript/publications/store/index.js
@@ -2,8 +2,10 @@ import { combineReducers } from 'redux';
 
 import { reducer as createPublicationForm } from './createPublicationForm';
 import { reducer as indexFindPublications } from './indexFindPublications';
+import { reducer as showFindPublication } from './showFindPublication';
 
 export default combineReducers({
   createPublicationForm,
   indexFindPublications,
+  showFindPublication,
 });
