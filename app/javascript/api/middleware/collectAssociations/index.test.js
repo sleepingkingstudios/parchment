a43b6684fd9f25372ce2445961c26fb1be6c4d4e
Rob Smith <merlin@sleepingkingstudios.com>
2019-10-02 23:09:47 -0400

    Update collectAssociations to handle missing data.

--- app/javascript/api/middleware/collectAssociations/index.test.js
+++ app/javascript/api/middleware/collectAssociations/index.test.js
@@ -58,42 +58,76 @@ describe('collectAssociations request middleware', () => {
       });
 
       describe('when the resource has an association', () => {
-        const book = {
-          id: '00000000-0000-0000-0000-000000000000',
-          name: "The Flumph Fancier's Handbook",
-        };
-        const spell = {
-          id: '10000000-0000-0000-0000-000000000000',
-          name: 'Conjure Flumph',
-          source_id: book.id,
-          source_type: 'Book',
-        };
-        const data = {
-          book,
-          spell,
-        };
+        describe('when the association is missing', () => {
+          const spell = {
+            id: '10000000-0000-0000-0000-000000000000',
+            name: 'Conjure Flumph',
+            source_id: '00000000-0000-0000-0000-000000000000',
+            source_type: 'Book',
+          };
+          const data = { spell };
 
-        it('should modify the response', () => {
-          const next = jest.fn();
-          const dispatch = jest.fn();
-          const getState = jest.fn();
-          const response = { json: { data } };
-          const expectedData = {
+          it('should modify the response', () => {
+            const next = jest.fn();
+            const dispatch = jest.fn();
+            const getState = jest.fn();
+            const response = { json: { data } };
+            const expectedData = {
+              spell: {
+                id: '10000000-0000-0000-0000-000000000000',
+                name: 'Conjure Flumph',
+                source: undefined,
+                source_id: '00000000-0000-0000-0000-000000000000',
+                source_type: 'Book',
+              },
+            };
+
+            const expectedResponse = { json: { data: expectedData } };
+
+            handleSuccess(next)({ dispatch, getState, response });
+
+            expect(next).toHaveBeenCalledWith({ dispatch, getState, response: expectedResponse });
+          });
+        });
+
+        describe('when the association is present', () => {
+          const book = {
+            id: '00000000-0000-0000-0000-000000000000',
+            name: "The Flumph Fancier's Handbook",
+          };
+          const spell = {
+            id: '10000000-0000-0000-0000-000000000000',
+            name: 'Conjure Flumph',
+            source_id: book.id,
+            source_type: 'Book',
+          };
+          const data = {
             book,
-            spell: {
-              id: '10000000-0000-0000-0000-000000000000',
-              name: 'Conjure Flumph',
-              source: book,
-              source_id: book.id,
-              source_type: 'Book',
-            },
+            spell,
           };
 
-          const expectedResponse = { json: { data: expectedData } };
+          it('should modify the response', () => {
+            const next = jest.fn();
+            const dispatch = jest.fn();
+            const getState = jest.fn();
+            const response = { json: { data } };
+            const expectedData = {
+              book,
+              spell: {
+                id: '10000000-0000-0000-0000-000000000000',
+                name: 'Conjure Flumph',
+                source: book,
+                source_id: book.id,
+                source_type: 'Book',
+              },
+            };
 
-          handleSuccess(next)({ dispatch, getState, response });
+            const expectedResponse = { json: { data: expectedData } };
 
-          expect(next).toHaveBeenCalledWith({ dispatch, getState, response: expectedResponse });
+            handleSuccess(next)({ dispatch, getState, response });
+
+            expect(next).toHaveBeenCalledWith({ dispatch, getState, response: expectedResponse });
+          });
         });
       });
     });
@@ -112,55 +146,6 @@ describe('collectAssociations request middleware', () => {
 
     describe('handleSuccess()', () => {
       const { handleSuccess } = middleware;
-      const books = [
-        {
-          id: '00000000-0000-0000-0000-000000000000',
-          name: "The Flumph Fancier's Handbook",
-        },
-      ];
-      const periodicals = [
-        {
-          id: '10000000-0000-0000-0000-000000000000',
-          issue: 12,
-          name: 'Flumph Monthly',
-        },
-      ];
-      const webArticles = [
-        {
-          id: '30000000-0000-0000-0000-000000000000',
-          url: 'flumphs.com/flumph-magic',
-        },
-      ];
-      const spells = [
-        {
-          id: '20000000-0000-0000-0000-000000000000',
-          name: 'Conjure Flumph',
-          source_id: books[0].id,
-          source_type: 'Book',
-        },
-        {
-          id: '20000000-0000-0000-0000-000000000001',
-          name: 'Flumph Bolt',
-          source_id: periodicals[0].id,
-          source_type: 'Periodical',
-        },
-        {
-          id: '20000000-0000-0000-0000-000000000002',
-          name: 'Magic Circle Against Flumphs',
-          source_id: webArticles[0].id,
-          source_type: 'WebArticle',
-        },
-        {
-          id: '20000000-0000-0000-0000-000000000003',
-          name: 'Transform Flumph',
-        },
-      ];
-      const data = {
-        books,
-        periodicals,
-        spells,
-        web_articles: webArticles,
-      };
 
       it('should be a function', () => {
         expect(typeof handleSuccess).toEqual('function');
@@ -172,49 +157,248 @@ describe('collectAssociations request middleware', () => {
         expect(typeof handleSuccess(next)).toEqual('function');
       });
 
-      it('should modify the response', () => {
-        const next = jest.fn();
-        const dispatch = jest.fn();
-        const getState = jest.fn();
-        const response = { json: { data } };
-        const expectedData = {
-          books,
-          periodicals,
-          spells: [
+      describe('when the resources do not have associations', () => {
+        const spells = [
+          {
+            id: '20000000-0000-0000-0000-000000000000',
+            name: 'Conjure Flumph',
+          },
+          {
+            id: '20000000-0000-0000-0000-000000000001',
+            name: 'Flumph Bolt',
+          },
+          {
+            id: '20000000-0000-0000-0000-000000000002',
+            name: 'Magic Circle Against Flumphs',
+          },
+          {
+            id: '20000000-0000-0000-0000-000000000003',
+            name: 'Transform Flumph',
+          },
+        ];
+        const data = { spells };
+
+        it('should modify the response', () => {
+          const next = jest.fn();
+          const dispatch = jest.fn();
+          const getState = jest.fn();
+          const response = { json: { data } };
+          const expectedData = {
+            spells: [
+              {
+                id: '20000000-0000-0000-0000-000000000000',
+                name: 'Conjure Flumph',
+                source: undefined,
+              },
+              {
+                id: '20000000-0000-0000-0000-000000000001',
+                name: 'Flumph Bolt',
+                source: undefined,
+              },
+              {
+                id: '20000000-0000-0000-0000-000000000002',
+                name: 'Magic Circle Against Flumphs',
+                source: undefined,
+              },
+              {
+                id: '20000000-0000-0000-0000-000000000003',
+                name: 'Transform Flumph',
+                source: undefined,
+              },
+            ],
+          };
+
+          const expectedResponse = { json: { data: expectedData } };
+
+          handleSuccess(next)({ dispatch, getState, response });
+
+          expect(next).toHaveBeenCalledWith({ dispatch, getState, response: expectedResponse });
+        });
+      });
+
+      describe('when the resources have associations', () => {
+        describe('when the associations are missing', () => {
+          const books = [
+            {
+              id: '00000000-0000-0000-0000-000000000000',
+              name: "The Flumph Fancier's Handbook",
+            },
+          ];
+          const spells = [
             {
               id: '20000000-0000-0000-0000-000000000000',
               name: 'Conjure Flumph',
-              source: books[0],
               source_id: books[0].id,
               source_type: 'Book',
             },
             {
               id: '20000000-0000-0000-0000-000000000001',
               name: 'Flumph Bolt',
-              source: periodicals[0],
+              source_id: '10000000-0000-0000-0000-000000000000',
+              source_type: 'Periodical',
+            },
+            {
+              id: '20000000-0000-0000-0000-000000000002',
+              name: 'Magic Circle Against Flumphs',
+              source_id: '30000000-0000-0000-0000-000000000000',
+              source_type: 'WebArticle',
+            },
+            {
+              id: '20000000-0000-0000-0000-000000000003',
+              name: 'Transform Flumph',
+              source_id: '',
+              source_type: '',
+            },
+          ];
+          const data = {
+            books,
+            periodicals: [],
+            spells,
+          };
+
+          it('should modify the response', () => {
+            const next = jest.fn();
+            const dispatch = jest.fn();
+            const getState = jest.fn();
+            const response = { json: { data } };
+            const expectedData = {
+              books,
+              periodicals: [],
+              spells: [
+                {
+                  id: '20000000-0000-0000-0000-000000000000',
+                  name: 'Conjure Flumph',
+                  source: books[0],
+                  source_id: books[0].id,
+                  source_type: 'Book',
+                },
+                {
+                  id: '20000000-0000-0000-0000-000000000001',
+                  name: 'Flumph Bolt',
+                  source: undefined,
+                  source_id: '10000000-0000-0000-0000-000000000000',
+                  source_type: 'Periodical',
+                },
+                {
+                  id: '20000000-0000-0000-0000-000000000002',
+                  name: 'Magic Circle Against Flumphs',
+                  source: undefined,
+                  source_id: '30000000-0000-0000-0000-000000000000',
+                  source_type: 'WebArticle',
+                },
+                {
+                  id: '20000000-0000-0000-0000-000000000003',
+                  name: 'Transform Flumph',
+                  source: undefined,
+                  source_id: '',
+                  source_type: '',
+                },
+              ],
+            };
+            const expectedResponse = { json: { data: expectedData } };
+
+            handleSuccess(next)({ dispatch, getState, response });
+
+            expect(next).toHaveBeenCalledWith({ dispatch, getState, response: expectedResponse });
+          });
+        });
+
+        describe('when the associations are present', () => {
+          const books = [
+            {
+              id: '00000000-0000-0000-0000-000000000000',
+              name: "The Flumph Fancier's Handbook",
+            },
+          ];
+          const periodicals = [
+            {
+              id: '10000000-0000-0000-0000-000000000000',
+              issue: 12,
+              name: 'Flumph Monthly',
+            },
+          ];
+          const webArticles = [
+            {
+              id: '30000000-0000-0000-0000-000000000000',
+              url: 'flumphs.com/flumph-magic',
+            },
+          ];
+          const spells = [
+            {
+              id: '20000000-0000-0000-0000-000000000000',
+              name: 'Conjure Flumph',
+              source_id: books[0].id,
+              source_type: 'Book',
+            },
+            {
+              id: '20000000-0000-0000-0000-000000000001',
+              name: 'Flumph Bolt',
               source_id: periodicals[0].id,
               source_type: 'Periodical',
             },
             {
               id: '20000000-0000-0000-0000-000000000002',
               name: 'Magic Circle Against Flumphs',
-              source: webArticles[0],
               source_id: webArticles[0].id,
               source_type: 'WebArticle',
             },
             {
               id: '20000000-0000-0000-0000-000000000003',
               name: 'Transform Flumph',
-              source: undefined,
             },
-          ],
-          web_articles: webArticles,
-        };
-        const expectedResponse = { json: { data: expectedData } };
+          ];
+          const data = {
+            books,
+            periodicals,
+            spells,
+            web_articles: webArticles,
+          };
 
-        handleSuccess(next)({ dispatch, getState, response });
+          it('should modify the response', () => {
+            const next = jest.fn();
+            const dispatch = jest.fn();
+            const getState = jest.fn();
+            const response = { json: { data } };
+            const expectedData = {
+              books,
+              periodicals,
+              spells: [
+                {
+                  id: '20000000-0000-0000-0000-000000000000',
+                  name: 'Conjure Flumph',
+                  source: books[0],
+                  source_id: books[0].id,
+                  source_type: 'Book',
+                },
+                {
+                  id: '20000000-0000-0000-0000-000000000001',
+                  name: 'Flumph Bolt',
+                  source: periodicals[0],
+                  source_id: periodicals[0].id,
+                  source_type: 'Periodical',
+                },
+                {
+                  id: '20000000-0000-0000-0000-000000000002',
+                  name: 'Magic Circle Against Flumphs',
+                  source: webArticles[0],
+                  source_id: webArticles[0].id,
+                  source_type: 'WebArticle',
+                },
+                {
+                  id: '20000000-0000-0000-0000-000000000003',
+                  name: 'Transform Flumph',
+                  source: undefined,
+                },
+              ],
+              web_articles: webArticles,
+            };
+            const expectedResponse = { json: { data: expectedData } };
 
-        expect(next).toHaveBeenCalledWith({ dispatch, getState, response: expectedResponse });
+            handleSuccess(next)({ dispatch, getState, response });
+
+            expect(next).toHaveBeenCalledWith({ dispatch, getState, response: expectedResponse });
+          });
+        });
       });
     });
   });
