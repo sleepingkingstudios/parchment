7ed3bdc2f202d8e2081360c70526c704332021ff
Rob Smith <merlin@sleepingkingstudios.com>
2019-11-04 12:30:31 -0500

    Extract Operations::Records::ParameterValidations.

--- lib/operations/records/base_operation.rb
+++ lib/operations/records/base_operation.rb
@@ -1,14 +1,13 @@
 # frozen_string_literal: true
 
-require 'errors/invalid_parameters'
-require 'errors/invalid_record'
-require 'errors/unknown_attributes'
 require 'operations/records'
+require 'operations/records/parameter_validations'
 require 'operations/steps'
 
 module Operations::Records
   # Abstract base class for record operations.
   class BaseOperation < Cuprum::Operation
+    include Operations::Records::ParameterValidations
     include Operations::Steps
 
     class << self
@@ -48,44 +47,5 @@ module Operations::Records
     # @return [Class] the class of record that the operation's business logic
     #   operates on.
     attr_reader :record_class
-
-    private
-
-    def handle_invalid_attributes(attributes)
-      return if attributes.is_a?(Hash)
-
-      error = Errors::InvalidParameters.new(
-        errors: [['attributes', 'must be a Hash']]
-      )
-
-      failure(error)
-    end
-
-    def handle_invalid_record(record)
-      return if record.is_a?(record_class)
-
-      error = Errors::InvalidRecord.new(record_class: record_class)
-
-      failure(error)
-    end
-
-    def handle_unknown_attribute
-      yield
-    rescue ActiveModel::UnknownAttributeError => exception
-      error = Errors::UnknownAttributes.new(
-        attributes:   [unknown_attribute_name(exception)],
-        record_class: record_class
-      )
-
-      failure(error)
-    end
-
-    def unknown_attribute_name(exception)
-      unknown_attribute_pattern.match(exception.message)['attribute_name']
-    end
-
-    def unknown_attribute_pattern
-      /unknown attribute '(?<attribute_name>.*)'/
-    end
   end
 end
--- lib/operations/records/find_one_operation.rb
+++ lib/operations/records/find_one_operation.rb
@@ -3,42 +3,15 @@
 require 'errors/invalid_parameters'
 require 'errors/not_found'
 require 'operations/records/base_operation'
+require 'operations/records/parameter_validations'
 
 module Operations::Records
   # Queries the database for the record in the given table with the given
   # primary key.
   class FindOneOperation < Operations::Records::BaseOperation
+    include Operations::Records::ParameterValidations::One
+
     private
-
-    def handle_empty_id(id)
-      return unless id.empty?
-
-      error = Errors::InvalidParameters.new(
-        errors: [['id', "can't be blank"]]
-      )
-
-      failure(error)
-    end
-
-    def handle_id_type_invalid(id)
-      return if id.is_a?(String)
-
-      error = Errors::InvalidParameters.new(
-        errors: [['id', 'must be a String']]
-      )
-
-      failure(error)
-    end
-
-    def handle_nil_id(id)
-      return unless id.nil?
-
-      error = Errors::InvalidParameters.new(
-        errors: [['id', "can't be blank"]]
-      )
-
-      failure(error)
-    end
 
     def find_record(id)
       record_class.find(id)
@@ -52,9 +25,7 @@ module Operations::Records
     end
 
     def process(id)
-      step :handle_nil_id,          id
-      step :handle_id_type_invalid, id
-      step :handle_empty_id,        id
+      step :handle_invalid_id, id
 
       find_record(id)
     end
