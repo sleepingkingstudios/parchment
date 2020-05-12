46d21375dc6022bed423d6038753374ba5084f5c
Rob Smith <merlin@sleepingkingstudios.com>
2020-05-11 20:48:43 -0400

    Implement authorization request middleware.

---
+++ app/javascript/api/middleware/authorization/index.js
@@ -0,0 +1,31 @@
+import { clearSession } from '../../../authentication/store/session/actions';
+import { dig, exists } from '../../../utils/object';
+
+const authorization = {
+  buildRequest: next => ({ getState, method, namespace }) => {
+    const state = getState();
+    const token = dig(state, 'authentication', 'session', 'token');
+    const request = next({ getState, method, namespace });
+
+    if (!exists(token)) { return request; }
+
+    const headers = Object.assign(
+      {},
+      request.headers,
+      { Authorization: `BEARER ${token}` },
+    );
+
+    return Object.assign({}, request, { headers });
+  },
+  handleFailure: next => ({ dispatch, getState, response }) => {
+    const { status } = response;
+
+    next({ dispatch, getState, response });
+
+    if (!(status === 401 || status === 403)) { return; }
+
+    dispatch(clearSession());
+  },
+};
+
+export default authorization;
---
+++ app/javascript/api/middleware/authorization/index.test.js
@@ -0,0 +1,153 @@
+import { clearSession } from '../../../authentication/store/session/actions';
+import authorization from './index';
+
+describe('authorization request middleware', () => {
+  const {
+    buildRequest,
+    handleFailure,
+  } = authorization;
+
+  describe('buildRequest()', () => {
+    const method = 'GET';
+    const namespace = 'api/endpoint';
+
+    it('should be a function', () => {
+      expect(typeof buildRequest).toEqual('function');
+    });
+
+    it('should return a function', () => {
+      const next = jest.fn();
+
+      expect(typeof buildRequest(next)).toEqual('function');
+    });
+
+    describe('when the session token is not set', () => {
+      const getState = jest.fn();
+      const rawRequest = { body: '', headers: { key: 'value' } };
+      const next = jest.fn(() => rawRequest);
+
+      it('should return the unmodified request', () => {
+        const request = buildRequest(next)({ getState, method, namespace });
+
+        expect(request).toEqual(rawRequest);
+      });
+
+      it('should call the next function', () => {
+        buildRequest(next)({ getState, method, namespace });
+
+        expect(next).toHaveBeenCalledWith({ getState, method, namespace });
+      });
+    });
+
+    describe('when the session token is set', () => {
+      const token = 'a.b.c';
+      const state = { authentication: { session: { token } } };
+      const getState = jest.fn(() => state);
+      const rawRequest = { body: '', headers: { key: 'value' } };
+      const next = jest.fn(() => rawRequest);
+
+      it('should add the authorization header to the request', () => {
+        const expected = Object.assign(
+          {},
+          rawRequest,
+          {
+            headers: Object.assign(
+              {},
+              rawRequest.headers,
+              { Authorization: `BEARER ${token}` },
+            ),
+          },
+        );
+        const request = buildRequest(next)({ getState, method, namespace });
+
+        expect(request).toEqual(expected);
+      });
+
+      it('should call the next function', () => {
+        buildRequest(next)({ getState, method, namespace });
+
+        expect(next).toHaveBeenCalledWith({ getState, method, namespace });
+      });
+    });
+  });
+
+  describe('handleFailure()', () => {
+    it('should be a function', () => {
+      expect(typeof handleFailure).toEqual('function');
+    });
+
+    it('should return a function', () => {
+      const next = jest.fn();
+
+      expect(typeof handleFailure(next)).toEqual('function');
+    });
+
+    describe('when the failure status is not 401 or 403', () => {
+      const getState = jest.fn();
+      const response = { status: 422 };
+      const next = jest.fn();
+
+      it('should not dispatch an action', () => {
+        const dispatch = jest.fn();
+
+        handleFailure(next)({ dispatch, getState, response });
+
+        expect(dispatch).not.toHaveBeenCalled();
+      });
+
+      it('should call the next function', () => {
+        const dispatch = jest.fn();
+
+        handleFailure(next)({ dispatch, getState, response });
+
+        expect(next).toHaveBeenCalledWith({ dispatch, getState, response });
+      });
+    });
+
+    describe('when the failure status is 401', () => {
+      const getState = jest.fn();
+      const response = { status: 401 };
+      const next = jest.fn();
+
+      it('should dispatch a clearSession action', () => {
+        const dispatch = jest.fn();
+        const expected = clearSession();
+
+        handleFailure(next)({ dispatch, getState, response });
+
+        expect(dispatch).toHaveBeenCalledWith(expected);
+      });
+
+      it('should call the next function', () => {
+        const dispatch = jest.fn();
+
+        handleFailure(next)({ dispatch, getState, response });
+
+        expect(next).toHaveBeenCalledWith({ dispatch, getState, response });
+      });
+    });
+
+    describe('when the failure status is 403', () => {
+      const getState = jest.fn();
+      const response = { status: 403 };
+      const next = jest.fn();
+
+      it('should dispatch a clearSession action', () => {
+        const dispatch = jest.fn();
+        const expected = clearSession();
+
+        handleFailure(next)({ dispatch, getState, response });
+
+        expect(dispatch).toHaveBeenCalledWith(expected);
+      });
+
+      it('should call the next function', () => {
+        const dispatch = jest.fn();
+
+        handleFailure(next)({ dispatch, getState, response });
+
+        expect(next).toHaveBeenCalledWith({ dispatch, getState, response });
+      });
+    });
+  });
+});
