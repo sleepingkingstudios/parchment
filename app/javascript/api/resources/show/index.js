64a4413b9868220ce06229c8d4b1ca3e9384fa16
Rob Smith <merlin@sleepingkingstudios.com>
2020-05-24 02:57:32 -0400

    Implement generic resource show endpoint.

---
+++ app/javascript/api/resources/show/index.js
@@ -0,0 +1,69 @@
+import pluralize from 'pluralize';
+
+import FindOneEndpoint from '../../findOne';
+import authorization from '../../middleware/authorization';
+import generateAlerts from '../../middleware/alerts';
+import generateRedirectToIndex from '../../middleware/redirectToIndex';
+import { injectMiddleware } from '../../middleware/utils';
+import {
+  exists,
+  valueOrDefault,
+} from '../../../utils/object';
+import { capitalize } from '../../../utils/string';
+
+const generateNamespace = (options) => {
+  if (exists(options.namespace)) { return options.namespace; }
+
+  const { resourceName } = options;
+
+  return `${pluralize(resourceName)}/showFind${capitalize(resourceName)}`;
+};
+
+const generateUrl = (options) => {
+  const { namespace } = options;
+
+  if (exists(options.url)) { return options.url; }
+
+  return `/api/${namespace.replace(/\/\w+$/, '')}/:id`;
+};
+
+const showEndpoint = (options) => {
+  const { resourceName } = options;
+  const data = valueOrDefault(options.data, {});
+  const namespace = generateNamespace(options);
+  const url = generateUrl({ ...options, namespace });
+
+  const alerts = generateAlerts({
+    action: 'find',
+    resourceName,
+    failure: true,
+  });
+  const redirect = generateRedirectToIndex({
+    baseUrl: `/${namespace.replace(/\/\w+$/, '')}`,
+    resourceName,
+    on: 'failure',
+  });
+  const middleware = injectMiddleware(
+    [
+      authorization,
+      alerts,
+      redirect,
+    ],
+    valueOrDefault(options.middleware, []),
+  );
+
+  const endpoint = new FindOneEndpoint({
+    data,
+    middleware,
+    namespace,
+    url,
+  });
+
+  endpoint.middleware = middleware;
+  endpoint.options = options;
+  endpoint.type = 'api/resources/show';
+
+  return endpoint;
+};
+
+export default showEndpoint;
